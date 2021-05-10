51a383be2180522919cec47430eaad6bda19f702
Alexander Park <parkalexser@gmail.com>
2021-05-10 18:00:16 +0500

    test keyboard

--- routes/botman.php
+++ routes/botman.php
@@ -1,12 +1,20 @@
 <?php
 use App\Http\Controllers\BotManController;
+use BotMan\Drivers\Telegram\Extensions\Keyboard;
+use BotMan\Drivers\Telegram\Extensions\KeyboardButton;
 
 $botman = resolve('botman');
 // $botman = app('botman');
 
 
 $botman->hears('foo', function($bot){
-	$bot->reply('bar');
+	Keyboard::create()->type( Keyboard::TYPE_KEYBOARD )
+	  ->oneTimeKeyboard(true)
+	  ->addRow( 
+	     KeyboardButton::create("Да")->callbackData('first_inline'),
+	     KeyboardButton::create("Нет")->callbackData('second_inline')
+	  )
+	  ->toArray();
 });
 
 $botman->hears('info', function ($bot) {
@@ -14,5 +22,8 @@ $botman->hears('info', function ($bot) {
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+
+
 
 ?>
