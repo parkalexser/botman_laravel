a835ffaf818169ab35462684bba29d4d54843b2d
Alexander Park <parkalexser@gmail.com>
2021-05-11 09:55:18 +0500

    keyboard test

--- routes/botman.php
+++ routes/botman.php
@@ -7,16 +7,19 @@ $botman = resolve('botman');
 // $botman = app('botman');
 
 
-$botman->hears('foo', function($bot){
+$botman->hears('hi', function($bot){
 	$bot->reply('Hello');
-	$bot->ask('Hello! What is your firstname?', function(Answer $answer) {
-    // Save result
-    // $this->firstname = $answer->getText();
-
-    // $this->say('Nice to meet you '.$this->firstname);
-    // $this->askEmail();
-});
-	
+	$bot->ask('What is your name', function ($answer, $conversation){
+        $conversation->say('Nice to meet you '.$answer->getText());
+    },
+        Keyboard::create()->type( Keyboard::TYPE_KEYBOARD )
+        ->oneTimeKeyboard(true)
+        ->addRow(
+            KeyboardButton::create("Да")->callbackData('first_inline'),
+            KeyboardButton::create("Нет")->callbackData('second_inline')
+        )
+        ->toArray()
+    );
 });
 
 $botman->hears('info', function ($bot) {
