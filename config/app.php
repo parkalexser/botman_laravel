e219d07c5c6b5d29cad379f41603621debdd3d3b
Alexander Park <parkalexser@gmail.com>
2021-05-12 10:53:26 +0500

    add tg service provider

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \BotMan\Drivers\Telegram\Providers\TelegramServiceProvider::class,
 
     ],
 
